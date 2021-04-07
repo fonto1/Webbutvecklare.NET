a58dc68dd829d206a33978b12d246f7bd13fb447
fonto1 <fonto_85@hotmail.com>
2021-04-07 23:35:21 +0200

    removed folder

--- 7. Lia1/src/Redux/Actions/rowAction.tsx
+++
@@ -1,7 +0,0 @@
-export const getAllRows = () => {
-  return async (dispatch: any) => {
-    const response = await fetch("http://localhost:4000/row");
-    const data = await response.json();
-    dispatch({ type: "SET_ALL_ROWS", payload: data });
-  };
-};
